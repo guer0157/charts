1f3c06d6ffe36b56851c728bdc249bedced1d4b8
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-14 12:12:16 -0500

    ewge
    ewgewgwg

--- js/info.js
+++ js/info.js
@@ -1,6 +1,9 @@
-var frame = document.getElementById("myChart");
-var graph = new Chart(frame, {
+let frame = document.getElementById("myChart");
+let data=[10,20,30]
+let labels=['Red','Yellow','Blue']
+let graph = new Chart(frame, {
     type:'pie',
+    data:data,
+    labels:labels,
     
-    
-})
+    })
