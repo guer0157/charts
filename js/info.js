59e9763a292f645f838957fb0f6901691bc2bb9b
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-14 22:58:49 -0500

    changes
    changes

--- js/info.js
+++ js/info.js
@@ -1,9 +1,8 @@
-let frame = document.getElementById("myChart");
+let frame = document.getElementById("myChart").getContext('2d');
 let data=[10,20,30]
 let labels=['Red','Yellow','Blue']
 let graph = new Chart(frame, {
     type:'pie',
     data:data,
-    labels:labels,
-    
+    labels:labels
     })
